3355adb9e1f153355c7d78c161c5c6651a05bd7e
sakuraba-coder <64248761+sakuraba-coder@users.noreply.github.com>
2022-08-13 17:07:06 +0900

    コメント追加

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
+
 import { CompleteTodoes } from "./components/CompleteTodoes";
 import { IncompleteTodoes } from "./components/IncompleteTodoes";
 import { InputTodo } from "./components/InputTodo";
 import "./styles.css";
 
 export const App = () => {
+  // Hooks useStateは初期値を表す
   const [todoText, setTodoText] = useState("");
   const [incompleteTodoes, setIncompleteTodoes] = useState([]);
   const [completeTodoes, setCompleteTodoes] = useState([]);
 
+  // テキストを入力する時はこれを必ず使う
+  // inputboxとセットだと言うことを覚えておこう
   const onChangeTodoText = (event) => {
     setTodoText(event.target.value);
   };
 
+  // inputboxに書いたことをincompleteTodoes配列に結合
+  // 変更があると更新される
   const onClickAdd = () => {
     if (todoText === "") return;
     const newTodoes = [...incompleteTodoes, todoText];
@@ -20,12 +26,17 @@ export const App = () => {
     setTodoText("");
   };
 
+  // 削除する際にはspliceを使う
+  // splice(index, 個数)
+  // indexから個数分削除する
   const onClickDelete = (index) => {
     const newTodoes = [...incompleteTodoes];
     newTodoes.splice(index, 1);
     setIncompleteTodoes(newTodoes);
   };
 
+  // 未完了配列から削除
+  // 完了配列へ追加
   const onClickComplete = (index) => {
     const newIncompleteTodoes = [...incompleteTodoes];
     newIncompleteTodoes.splice(index, 1);
@@ -35,6 +46,8 @@ export const App = () => {
     setCompleteTodoes(newCompleteTodoes);
   };
 
+  // 完了配列から削除
+  // 未完了配列へ追加
   const onClickBack = (index) => {
     const newCompleteTodoes = [...completeTodoes];
     newCompleteTodoes.splice(index, 1);
@@ -44,6 +57,8 @@ export const App = () => {
     setCompleteTodoes(newCompleteTodoes);
   };
 
+  // returnの中身が更新される
+  // ここにコンポーネントを配置していけばいいのではないか？
   return (
     <>
       <InputTodo
@@ -52,6 +67,7 @@ export const App = () => {
         onClick={onClickAdd}
         disabled={incompleteTodoes.length >= 5}
       />
+
       {incompleteTodoes.length >= 5 && (
         <p style={{ color: "red" }}>
           登録できるtodoは5個までです。消化しましょう。
